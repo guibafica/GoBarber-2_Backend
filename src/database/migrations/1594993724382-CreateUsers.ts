acd0b2bdc0ba74dd312f0577454ec249c68a4207
Guilherme Bafica <gb.engcomp@hotmail.com>
2020-07-17 11:57:40 -0300

    ADD: Table Users e Relacionamento Users_Appointments

--- src/database/migrations/1594993724382-CreateUsers.ts
+++ src/database/migrations/1594993724382-CreateUsers.ts
@@ -1,12 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
-import { name } from 'express';
 
-export default class CreateAppointmentsN1589329124990
-  implements MigrationInterface {
+export default class CreateUsers1594993724382 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -16,12 +14,17 @@ export default class CreateAppointmentsN1589329124990
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
           },
           {
             name: 'created_at',
@@ -39,6 +42,6 @@ export default class CreateAppointmentsN1589329124990
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,4 +1,12 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import User from './User';
 
 @Entity('appointments') // Para armazenar nesta tabela
 class Appointment {
@@ -6,10 +14,18 @@ class Appointment {
   id: string;
 
   @Column() // Por padrão, string
-  provider: string;
+  provider_id: string;
+
+  provider: User;
 
   @Column('timestamp with time zone')
   date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 
   // Ao criar uma Entity do typrorm, o constructor é gerado automaticamente
 }
