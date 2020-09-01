5f0a8c3d25bea8f1bee3a70eac61a5dc79a30558
Guilherme Bafica <gb.engcomp@hotmail.com>
2020-09-01 11:05:00 -0300

    CHORE: Liskov Substitution Principle

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,17 +1,20 @@
 import { EntityRepository, Repository } from 'typeorm';
 
-import Appointment from '../infra/typeorm/entities/Appointment';
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+
+import Appointment from '../entities/Appointment';
 
 @EntityRepository(Appointment)
-class AppointmentsRepository extends Repository<Appointment> {
-  public async findByDate(date: Date): Promise<Appointment | null> {
+class AppointmentsRepository extends Repository<Appointment>
+  implements IAppointmentsRepository {
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
     // O retorno de uma função async, é uma promise(Valor que pode estar
     // disponível agora, no futuro ou nunca)
     const findAppointment = await this.findOne({
       where: { date },
     });
 
-    return findAppointment || null;
+    return findAppointment;
   }
 }
 
