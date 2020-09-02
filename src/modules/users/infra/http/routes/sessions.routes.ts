2210d0ef92c2138de3f54664513522fe4707b435
Guilherme Bafica <gb.engcomp@hotmail.com>
2020-09-02 11:54:55 -0300

    CHORE: Refatorando modulo de usuarios

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
 
 const sessionsRouter = Router();
@@ -7,7 +8,8 @@ const sessionsRouter = Router();
 sessionsRouter.post('/', async (req, res) => {
   const { email, password } = req.body;
 
-  const authenticatedUser = new AuthenticateUserService();
+  const usersRepository = new UsersRepository();
+  const authenticatedUser = new AuthenticateUserService(usersRepository);
 
   const { user, token } = await authenticatedUser.execute({
     email,
