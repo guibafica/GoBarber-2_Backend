55538a016f40a3f8fedd983c455e8d4326c214be
Guilherme Bafica <gb.engcomp@hotmail.com>
2020-09-02 10:40:27 -0300

    CHORE: Reescreve repositorio

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,20 +1,36 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { getRepository, Repository } from 'typeorm';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import ICreateAppointmentDto from '@modules/appointments/dtos/ICreateAppointmentDTO';
 
 import Appointment from '../entities/Appointment';
 
-@EntityRepository(Appointment)
-class AppointmentsRepository extends Repository<Appointment>
-  implements IAppointmentsRepository {
+class AppointmentsRepository implements IAppointmentsRepository {
+  private ormRepository: Repository<Appointment>;
+
+  constructor() {
+    this.ormRepository = getRepository(Appointment);
+  }
+
   public async findByDate(date: Date): Promise<Appointment | undefined> {
     // O retorno de uma função async, é uma promise(Valor que pode estar
     // disponível agora, no futuro ou nunca)
-    const findAppointment = await this.findOne({
+    const findAppointment = await this.ormRepository.findOne({
       where: { date },
     });
 
     return findAppointment;
+  }
+
+  public async create({
+    provider_id,
+    date,
+  }: ICreateAppointmentDto): Promise<Appointment> {
+    const appointments = this.ormRepository.create({ provider_id, date });
+
+    await this.ormRepository.save(appointments);
+
+    return appointments;
   }
 }
 
