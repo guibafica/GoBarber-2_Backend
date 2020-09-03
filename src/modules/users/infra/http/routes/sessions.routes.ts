ff6934e2af58bec0894c62f44acf58cc1dce0c6d
Guilherme Bafica <gb.engcomp@hotmail.com>
2020-09-02 22:12:51 -0300

    ADD: Injeçao de dependencias

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
+import { container } from 'tsyringe';
 
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
 
 const sessionsRouter = Router();
@@ -8,8 +8,7 @@ const sessionsRouter = Router();
 sessionsRouter.post('/', async (req, res) => {
   const { email, password } = req.body;
 
-  const usersRepository = new UsersRepository();
-  const authenticatedUser = new AuthenticateUserService(usersRepository);
+  const authenticatedUser = container.resolve(AuthenticateUserService);
 
   const { user, token } = await authenticatedUser.execute({
     email,
