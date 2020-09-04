b7e3f222cad6da33672d77f02431837279d64317
Guilherme Bafica <gb.engcomp@hotmail.com>
2020-09-03 21:46:28 -0300

    CHORE: Cria primeiro teste

---
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -0,0 +1,41 @@
+import { uuid } from 'uuidv4';
+
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import ICreateAppointmentDto from '@modules/appointments/dtos/ICreateAppointmentDTO';
+
+import Appointment from '../../infra/typeorm/entities/Appointment';
+
+class AppointmentsRepository implements IAppointmentsRepository {
+  private appointments: Appointment[] = [];
+
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = this.appointments.find(
+      appointment => appointment.date === date,
+    );
+
+    return findAppointment;
+  }
+
+  public async create({
+    provider_id,
+    date,
+  }: ICreateAppointmentDto): Promise<Appointment> {
+    const appointment = new Appointment();
+
+    // Object.assign junta os valores. Isso substitui as linhas de baixo
+    Object.assign(appointment, { id: uuid(), date, provider_id });
+
+    // appointment.id = uuid();
+    // appointment.date = date;
+    // appointment.provider_id = provider_id;
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
+
+// findByDate(date).then() == const response = await findByDate(date)
+// .then() => Pega o resultado da função
+
+export default AppointmentsRepository;
