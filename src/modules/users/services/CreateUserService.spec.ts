86312723044f94e6c68cda4c82692f89f84814f7
Guilherme Bafica <gb.engcomp@hotmail.com>
2020-09-03 22:40:50 -0300

    ADD: Testes criaçao de usuario

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,38 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import CreateUsersService from './CreateUsersService';
+
+describe('CreateUser', () => {
+  it('should be able to create a new user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateUsersService(fakeUsersRepository);
+
+    const user = await createUser.execute({
+      name: 'John Doe', // John Doe = Fulano
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new user with same email from another', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateUsersService(fakeUsersRepository);
+
+    await createUser.execute({
+      name: 'John Doe', // John Doe = Fulano
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(
+      createUser.execute({
+        name: 'John Doe', // John Doe = Fulano
+        email: 'johndoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
