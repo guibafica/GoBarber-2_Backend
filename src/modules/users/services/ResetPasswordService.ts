303900868a4cebf346ced6ac58703cf1f336f1a7
Guilherme Bafica <gb.engcomp@hotmail.com>
2020-09-12 00:14:41 -0300

    CHORE: Reset senha 02:47

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,26 @@
+import { injectable, inject } from 'tsyringe';
+
+// import AppError from '@shared/errors/AppError';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
+
+// import User from '../infra/typeorm/entities/User';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class ResetPasswordService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
+  ) {}
+
+  public async execute({ token, email }: IRequest): Promise<void> {}
+}
+
+export default ResetPasswordService;
